03304cc0fb5ba115aa7548a76d75d2a070f2d35b
Kristoffer Bråthen Halden <>
2020-05-05 18:46:53 +0200

    Gruntfile and Sass Working properly

---
+++ dist/hoftun-utvikling.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+import Layout from './layout/Layout';
+
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement("p", null, "Hello from App"), /*#__PURE__*/React.createElement(Layout, null));
+}
+
+export default App;
+//# sourceMappingURL=hoftun-utvikling.js.map
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import Layout from './layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello from App</p>
+      <Layout>
+          
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
